58b2cef2e0e8c57da90d5419a748a4b5d7189264
jroquelaure <jonathanr@jfrog.com>
2018-09-14 11:42:11 +0200

    fixed version

--- sonarqube-scanner-gradle/src/main/java/App.java
+++ sonarqube-scanner-gradle/src/main/java/App.java
@@ -5,11 +5,17 @@ public class App {
     
     public String getGreeting() {
         
-       return "Hello world.";
+         StringBuilder message = new StringBuilder("Hello");
+        message.append(" ");
+        message.append(firstName);
+        message.append(" ");
+        message.append(lastName);
+
+          return message.toString();
     }
 
     public static void main(String[] args) {
         int i =0;
-        
     }
+
 }
