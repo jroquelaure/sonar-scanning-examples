d95a0e940bc0516a29550eaf8752b4af1419f287
jroquelaure <jonathanr@jfrog.com>
2018-09-14 12:01:02 +0200

    init string vars

--- sonarqube-scanner-gradle/src/main/java/App.java
+++ sonarqube-scanner-gradle/src/main/java/App.java
@@ -6,6 +6,15 @@ public class App {
     public String getGreeting(String firstName, String lastName) {
         
          StringBuilder message = new StringBuilder("Hello");
+
+        // Initializing names in case they're missing
+        if (firstName == null || lastName == null) {
+            BufferedReader reader = new BufferedReader(new FileReader("default-values.txt"));
+            firstName = reader.readLine();
+            lastName = reader.readLine();
+        } 
+
+
         message.append(" ");
         message.append(firstName);
         message.append(" ");
