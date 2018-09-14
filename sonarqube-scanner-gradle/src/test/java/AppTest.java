a31a8e093de2899d2dab97b386dc25458b2c3c5e
jroquelaure <jonathanr@jfrog.com>
2018-09-14 12:04:11 +0200

    init string vars

--- sonarqube-scanner-gradle/src/test/java/AppTest.java
+++ sonarqube-scanner-gradle/src/test/java/AppTest.java
@@ -6,7 +6,11 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
+         try {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting("Jon", "Roquelaure"));
+         } catch(IOException e) {
+            // nothing
+        }
     }
 }
